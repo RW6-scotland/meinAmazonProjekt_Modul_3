c7fa6110ff95d8261246cf06368a58df80b477e6
Raphael Walter <raphael.wal@outlook.com>
2024-10-08 11:33:46 +0200

    Nun habe ich die Medienbibliotheks Klasse erstellt. Danach habe ich eine ArrayListe vom Typ Medium erstellt. Zusätlich habe ich eine Methode mediumHinzufügen erstellt um die Liste mit Filme oder eBooks zu erweitern

--- src/Medienbibliothek.java
+++ src/Medienbibliothek.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Medienbibliothek {
+
+    private ArrayList<Medium> medien;
+
+    public void mediumHinzufügen(Medium a)
+    {
+        medien.add(a);
+    }
 }

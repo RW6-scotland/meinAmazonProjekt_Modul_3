e20fee5add716a0cac997b0b43c493dec0a09875
Raphael Walter <raphael.wal@outlook.com>
2024-10-08 11:12:20 +0200

    Nun habe ich die Klasen eBooks.java und Filme.java implemtiert. Sie erben beide von Medium. Somit müssen wir alle Parameter verwenden die im Mutterklassenkonstruktor auch verwendet worden sind. Zuzsätzlich hat jede Klasse noch eigene Datenfelder die wir im Parameter mitgeben. Danach haben wir bei beiden Klassen wieder die Setter und Getter generiert.

---
+++ src/Filme.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+
+public class Filme extends Medium{
+
+    private double spielzeit;
+
+    private String auflösung;
+
+
+    public Filme(String titel, Urheber urheber, Genre genre, String kommentar, LocalDate erscheinungsdatum,double spielzeit, String auflösung) {
+        super(titel, urheber, genre, kommentar, erscheinungsdatum);
+        setSpielzeit(spielzeit);
+        setAuflösung(auflösung);
+    }
+
+    public double getSpielzeit() {
+        return spielzeit;
+    }
+
+    public void setSpielzeit(double spielzeit) {
+        this.spielzeit = spielzeit;
+    }
+
+    public String getAuflösung() {
+        return auflösung;
+    }
+
+    public void setAuflösung(String auflösung) {
+        this.auflösung = auflösung;
+    }
+}
---
+++ src/eBooks.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+
+public class eBooks extends Medium{
+
+    private String isbn;
+
+    private int seitenanzahl;
+
+    private String auflage;
+
+    public eBooks(String titel, Urheber urheber, Genre genre, String kommentar, LocalDate erscheinungsdatum, String isbn, int seitenanzahl, String auflage)
+    {
+        super(titel, urheber, genre, kommentar, erscheinungsdatum);
+        setIsbn(isbn);
+        setSeitenanzahl(seitenanzahl);
+        setAuflage(auflage);
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getSeitenanzahl() {
+        return seitenanzahl;
+    }
+
+    public void setSeitenanzahl(int seitenanzahl) {
+        this.seitenanzahl = seitenanzahl;
+    }
+
+    public String getAuflage() {
+        return auflage;
+    }
+
+    public void setAuflage(String auflage) {
+        this.auflage = auflage;
+    }
+}
